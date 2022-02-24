38e23e753843cc470d7e1ea0e1ff35402b4483ba
Rohy Goudarzi <roham2002@yahoo.com>
2022-02-23 16:06:25 -0800

    Created board with shapes

---
+++ board.py
@@ -0,0 +1,49 @@
+import pygame
+import os
+
+# constants
+WIDTH, HEIGHT = 720, 720                     # constant width and height, set for basic testing   
+WIN = pygame.display.set_mode((WIDTH, HEIGHT)) # setting window width and height
+pygame.display.set_caption("SelfChessAI")      # setting name of window
+fps = 60                                       # setting fps of game
+
+DIMENSION = WIDTH/8
+
+class Square:
+    # default constructor
+    WHITE=(255,255,255)
+    BLACK=(0,0,0)
+    
+    def __init__(self, file, rank, color): 
+        self.file = file
+        self.rank = rank
+        self.isWhite = color
+        self.color = self.WHITE if self.isWhite else self.BLACK
+
+    def draw(self):
+        pygame.draw.rect(WIN, self.color, pygame.Rect((DIMENSION*(self.rank)), (HEIGHT-DIMENSION*(self.file+1)), DIMENSION, DIMENSION))
+        
+
+# function needed for refreshing window
+def drawwindow():
+    for file in range(8):
+        for rank in range(8):
+            isWhite = (file+rank)%2 != 0
+            Square(file, rank, isWhite).draw()
+    pygame.display.update()
+
+
+
+def main():
+    # running main window
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(fps)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        drawwindow()
+    pygame.quit()
+
+main()
--- game.py
+++ game.py
@@ -7,12 +7,23 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT)) # setting window width and height
 pygame.display.set_caption("SelfChessAI")      # setting name of window
 fps = 60                                       # setting fps of game
 
+WHITE=(255,255,255)
+BLACK=(0,0,0)
+
+BLACK_SQUARE = pygame.draw.rect(WIN, BLACK, pygame.Rect(30, 30, 60, 60),  2)
+WHITE_SQUARE = pygame.draw.rect(WIN, WHITE, pygame.Rect(30, 30, 60, 60),  2)
+# pygame.display.flip()
+    
+
 board = pygame.transform.scale(pygame.image.load(os.path.join('assets', 'board.png')), (500,500))
 
 # function needed for refreshing window
 def drawwindow():
-    WIN.blit(board, (0,0))
+    # WIN.blit(board, (0,0))
     pygame.display.update()
+    # pygame.draw.rect(WIN,BLUE,(200,150,100,50))
+
+
 
 def main():
     # running main window
